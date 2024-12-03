8980bc9d1948f905110f8f47fb8f1040983ebf5e
LuuuXXX <luuux98@163.com>
2024-12-03 17:08:02 +0800

    init ci

---
+++ ci/scripts/run-with-docker.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -e
+
+# linux
+image=""
+while [[ $# -gt 0 ]]
+do
+  case "$1" in
+    --dev)
+      dev=1
+      ;;
+    *)
+      if [ -n "$image" ]
+      then
+        echo "excepted single argument for the image value"
+        exit 1
+      fi
+      image="$1"
+      ;;
+  esac
+  shift
+done
+
+docker --version
+pwd
+ls -al
+
+source_dir="$(pwd)"
+docker_dir="ci/docker"
+
+if [ -f "$docker_dir/$image/Dockerfile" ]; then
+    dockerfile="$docker_dir/$image/Dockerfile"
+    # build docker image.
+    docker buildx build --rm -t rim-ci -f "$dockerfile" .
+else
+    echo "Invalid docker image: $image"
+fi
+
+# run ther docker image.
+docker run --workdir /checkout/obj -v "$source_dir:/checkout/obj" --init --rm rim-ci
---
+++ ci/scripts/run-without-docker.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+# windows
+export target="x86_64-pc-windows-msvc"
+command="cargo dev vendor"
+
+# install nodejs
+# mkdir -p $HOME/node
+# curl -sL https://nodejs.org/dist/v16.9.0/node-v16.9-linux-x64.tar.xz | tar -xJ -C $HOME/node --strip-components=1
+# export PATH="$HOME/node/bin:$PATH"
+
+echo "Execiting ${command}"
+sh -x -c "$command" 
